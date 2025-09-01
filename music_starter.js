bb96b7d2150285a1fcb3b5a85c20a87a654bd688
sqvart <68765049+sqvart@users.noreply.github.com>
2025-09-02 09:57:48 +1200

    test

--- music_starter.js
+++ music_starter.js
@@ -2,36 +2,41 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
-  textFont('Verdana'); // please use CSS safe fonts
+  textFont('Arial'); // please use CSS safe fonts
   rectMode(CENTER)
+  textAlign(CENTER)
   textSize(24);
   
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
- 
+   let vocalWidth = map(vocal, 0, 100, 0, width);
+   let drumWidth  = map(drum, 0, 100, 0, width);
+   let bassWidth  = map(bass, 0, 100, 0, width);
+   let otherWidth = map(other, 0, 100, 0, width);
+
 // changes 
    // vocal bar is red
    fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+   rect(bar_pos_x, height / 2 + 1 * bar_spacing, vocalWidth, bar_height);
    fill(0);
    text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
    // drum bar is green
    fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+   rect(bar_pos_x, height / 2 + 2 * bar_spacing, drumWidth, bar_height);
    fill(0);
    text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
    // bass bar is blue
    fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+   rect(bar_pos_x, height / 2 + 3 * bar_spacing, bassWidth, bar_height);
    fill(0);
    text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
    // other bar is white
    fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+   rect(bar_pos_x, height / 2 + 4 * bar_spacing, otherWidth, bar_height);
    fill(0);
    text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
    fill(255, 255, 0);
