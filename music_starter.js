37509d777dbc549071dc731f7485e81e622527be
sqvart <68765049+sqvart@users.noreply.github.com>
2025-09-09 10:21:05 +1200

    test

--- music_starter.js
+++ music_starter.js
@@ -5,13 +5,14 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+  angleMode(RADIANS)
   
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
-
+   let bassmap = map(bass, 0, 100, 20, 120)
+   let vocalmap = map(vocal, 0, 100, 30, 130)
 // changes 
-
 
    //background 
 
@@ -20,7 +21,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
    // vocal bar is red
    fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+   circle(width / 2, height / 2, vocalmap * 3, vocalmap * 3 );
    fill(0);
    text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
@@ -31,13 +32,21 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
    // bass bar is blue
-   noFill()
-   stroke(256)
-   circle(width / 2, height / 2, bass * 2, bass * 2);
+   fill(256)
+   noStroke(256)
+   circle(width / 2, height / 2, bassmap * 2, bassmap * 2);
    textAlign(CENTER);
    fill(0)
    text("bass", width / 2, height / 2);
- 
+
+    // bass bar 2
+   noFill()
+   stroke(256)
+   circle(width / 2, height / 2, bassmap * 2.5, bassmap * 2.5);
+   textAlign(CENTER);
+   fill(0)
+   text("bass", width / 2, height / 2);
+
    // other bar is white
    fill(200, 200, 200);
    rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
